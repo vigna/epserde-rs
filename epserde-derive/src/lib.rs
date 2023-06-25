e8397a7264d48859a3f6c8e941d9a4306d43b9f7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-06-26 00:12:50 +0200

    Working deserialize() for structs without type parameters

--- epserde-derive/src/lib.rs
+++ epserde-derive/src/lib.rs
@@ -11,6 +11,7 @@ pub fn epserde_serialize_derive(input: TokenStream) -> TokenStream {
         Data::Struct(s) => {
             let fields = s.fields.into_iter().map(|field| field.ident.unwrap());
             quote! {
+                #[automatically_derived]
                 impl epserde_trait::Serialize for #name {
                     fn serialize<F: std::io::Write + std::io::Seek>(&self, backend: &mut F) -> anyhow::Result<usize> {
                         let mut bytes = 0;
@@ -31,15 +32,28 @@ pub fn epserde_serialize_derive(input: TokenStream) -> TokenStream {
 pub fn epserde_deserialize_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
+    // We have to play with this to get type parameters working
+    let generics = input.generics.params.into_iter().collect::<Vec<_>>();
 
     let out = match input.data {
         Data::Struct(s) => {
-            let fields = s.fields.into_iter().map(|field| field.ident.unwrap());
+            let fields = s
+                .fields
+                .iter()
+                .map(|field| field.ident.to_owned().unwrap())
+                .collect::<Vec<_>>();
+            let types = s.fields.into_iter().map(|field| field.ty);
             quote! {
+                #[automatically_derived]
                 impl<'a> epserde_trait::Deserialize<'a> for #name {
                     fn deserialize(backend: &'a [u8]) -> anyhow::Result<(Self, &'a [u8])> {
                         let mut bytes = 0;
-                        Ok((#name::default(), backend))
+                        #(
+                             let(#fields, backend) = #types::deserialize(backend)?;
+                        )*
+                        Ok((#name{
+                            #(#fields),*
+                        }, backend))
                     }
                 }
 
--- test-epserde/src/main.rs
+++ test-epserde/src/main.rs
@@ -1,11 +1,21 @@
-use epserde_derive::Deserialize;
+use epserde_derive::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, PartialEq, Eq, Default)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Default)]
 struct Person {
     name: u64,
     age: u128,
 }
 
+use epserde_trait::{Deserialize, Serialize};
+
 fn main() {
-    println!("Hello, Derive");
+    let person0 = Person {
+        name: 0xdeadbeed,
+        age: 0xdeadbeefdeadf00d,
+    };
+    let mut v = vec![0; 100];
+    let mut buf = std::io::Cursor::new(&mut v);
+    person0.serialize(&mut buf).unwrap();
+    let (person1, _rest) = Person::deserialize(&v).unwrap();
+    assert_eq!(person0, person1);
 }
