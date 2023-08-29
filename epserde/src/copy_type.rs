96dc6b51ee9c8fb24a97831e690454dc4c675aee
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-08-29 17:21:14 +0200

    Documenting zero copy

--- epserde/src/copy_type.rs
+++ epserde/src/copy_type.rs
@@ -4,7 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-/*!
+/// Internal trait used to select whether a type is zero copy or not.
+/// It has only two implementations, [`Eps`] and [`Zero`].
+pub trait CopySelector {
+    const IS_ZERO_COPY: bool;
+}
+/// An implementation of a [`CopySelector`] specifying that a type is zero copy.
+pub struct Zero {}
+
+impl CopySelector for Zero {
+    const IS_ZERO_COPY: bool = true;
+}
+
+/// An implementation of a [`CopySelector`] specifying that a type is not zero copy.
+pub struct Eps {}
+
+impl CopySelector for Eps {
+    const IS_ZERO_COPY: bool = false;
+}
+
+/**
 
 Marker trait for data in vectors, boxes slices, or custom types
 that need to know whether a slice of data can be zero-copy deserialized.
@@ -24,7 +43,8 @@ impl CopyType for MyType {
 // Now MyType implements ZeroCopy
 ```
 
-We use this trait to implement a different behavior for [`ZeroCopy`] and [`EpsCopy`] types,
+We use this trait to implement a different behavior for [`ZeroCopy`] and [`EpsCopy`] types
+on arrays, vectors, and boxed slices,
 [working around the bug that prevents the compiler from understanding that implementations
 for the two flavors of `CopySelector` are mutually
 exclusive](https://github.com/rust-lang/rfcs/pull/1672#issuecomment-1405377983).
@@ -39,37 +59,33 @@ using the reference.
 You can implement `CopySelector<Type=Zero>` for your copy types, but you must ensure that the type does not
 contain references and has a fixed memory layout; for structures, this requires
 `repr(C)`. ε-serde will track these conditions at compile time and check them at
-runtime: in case of failure, serialization/deserialization will panic.
+runtime: in case of failure, serialization will panic.
 
 Since we cannot use negative trait bounds, every type that is used as a parameter of
-a vector or boxed slice must implement either `CopySelector<Type=Zero>` or `CopySelector<Type=Eps>`. In the latter
+an array, vector or boxed slice must implement either `CopySelector<Type=Zero>` or `CopySelector<Type=Eps>`. In the latter
 case, slices will be deserialized element by element, and the result will be a fully
 deserialized vector or boxed
 slice. If you do not implement either of these traits, the type will not be serializable inside
 vectors or boxed slices but error messages will be very unhelpful due to the
 contrived way we implement mutually exclusive types.
 
+If you use the provided derive macros all this logic will be hidden from you. You'll
+just have to add `#[zero_copy]` to your structures (if you want them to be zero-copy)
+and ε-serde will do the rest.
+
 */
-
-pub trait CopySelector {
-    const IS_ZERO_COPY: bool;
-}
-pub struct Eps {}
-pub struct Zero {}
-
-impl CopySelector for Eps {
-    const IS_ZERO_COPY: bool = false;
-}
-impl CopySelector for Zero {
-    const IS_ZERO_COPY: bool = true;
-}
 
 pub trait CopyType {
     type Copy: CopySelector;
 }
 
+/// Marker trait for zero-copy types. You should never implement
+/// this trait manually, but rather implement [`CopyType`] with `Copy=Zero`.
 pub trait ZeroCopy: CopyType<Copy = Zero> {}
 impl<T: CopyType<Copy = Zero>> ZeroCopy for T {}
+
+/// Marker trait for non zero-copy types. You should never implement
+/// this trait manually, but rather implement [`CopyType`] with `Copy=Eps`.
 pub trait EpsCopy: CopyType<Copy = Eps> {}
 impl<T: CopyType<Copy = Eps>> EpsCopy for T {}
 
