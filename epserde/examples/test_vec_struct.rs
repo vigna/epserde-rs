55d6fccff038cf9ec7afdcbcb3ae50c1966622e2
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-08-29 13:26:19 +0200

    Example for vecs of structs

---
+++ epserde/examples/test_vec_struct.rs
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Inria
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+use epserde::*;
+
+#[derive(Serialize, Deserialize, TypeHash, Debug, PartialEq, Eq, Default, Clone)]
+#[repr(C)]
+#[zero_copy]
+
+struct Data {
+    a: usize,
+}
+
+fn main() {
+    // Create a vector to serialize
+
+    let a = vec![Data { a: 5 }];
+
+    // Create an aligned vector to serialize into so we can do an ε-copy
+    // deserialization safely
+    let len = 100;
+    let mut v = unsafe {
+        Vec::from_raw_parts(
+            std::alloc::alloc_zeroed(std::alloc::Layout::from_size_align(len, 4096).unwrap()),
+            len,
+            len,
+        )
+    };
+    assert!(v.as_ptr() as usize % 4096 == 0, "{:p}", v.as_ptr());
+    // wrap the vector in a cursor so we can serialize into it
+    let mut buf = std::io::Cursor::new(&mut v);
+
+    // Serialize
+    let _bytes_written = a.serialize(&mut buf).unwrap();
+
+    // Do a full-copy deserialization
+    let full = Data::deserialize_full_copy(&v).unwrap();
+    println!(
+        "Full-deserialization type: {}",
+        std::any::type_name::<Data>(),
+    );
+    println!("Value: {:x?}", full);
+
+    println!("\n");
+
+    // Do an ε-copy deserialization
+    let eps = Data::deserialize_eps_copy(&v).unwrap();
+    println!(
+        "ε-deserialization type: {}",
+        std::any::type_name::<<Data as DeserializeInner>::DeserType<'_>>(),
+    );
+    println!("Value: {:x?}", eps);
+}
