72b8340fd8a971aefd5cf12c788c03a88792a0d0
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-09-17 00:38:02 +0200

    Docs

--- epserde/src/traits/type_info.rs
+++ epserde/src/traits/type_info.rs
@@ -5,6 +5,12 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
+/*!
+
+Traits computing structural information about a type.
+
+*/
+
 /// Compute a type hash and a representational hash for a type.
 /// They are used during deserialization to
 /// check that the type of the data matches the type of the value being
@@ -27,8 +33,10 @@ pub trait TypeHash {
     }
 }
 
-/// A trait giving the maximum size of a primitive field in a type,
-/// which is the padding used to store a zero-copy type. Note that this
+/// A trait giving the maximum size of a primitive field in a type.
+///
+/// We use the value returned by [`MaxSizeOf::max_size_of`]
+/// to generate padding before storing a zero-copy type. Note that this
 /// is different from the padding used to align the same type inside
 /// a struct, which is not under our control and is
 /// given by [`core::mem::align_of`].
