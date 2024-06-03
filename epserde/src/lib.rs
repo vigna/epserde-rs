b7c7a5456bf2e148edc540f3cd0c1f626eb5e012
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-06-03 19:18:34 +0200

    Fixed README link

--- epserde/src/lib.rs
+++ epserde/src/lib.rs
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-#![doc = env!("README")]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 #![deny(unconditional_recursion)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #[cfg(all(feature = "alloc", not(feature = "std")))]
