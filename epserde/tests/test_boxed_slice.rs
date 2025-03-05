965187fd8352bb3938b228a9c4ced51966cdab32
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-03-05 15:14:48 +0000

    Added test for boxed slices

---
+++ epserde/tests/test_boxed_slice.rs
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Inria
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+use anyhow::Result;
+use epserde::prelude::*;
+use maligned::A16;
+
+#[derive(Epserde, Debug, PartialEq, Eq, Clone)]
+struct Data<A: PartialEq = usize, const Q: usize = 3> {
+    a: A,
+    b: [i32; Q],
+}
+
+#[test]
+fn test_boxed_slices() -> Result<()> {
+    let a = vec![1, 2, 3, 4].into_boxed_slice();
+    let mut cursor = <AlignedCursor<A16>>::new();
+    a.serialize(&mut cursor)?;
+    cursor.set_position(0);
+    let b = Box::<[i32]>::deserialize_full(&mut cursor)?;
+    assert_eq!(a, b);
+    let b = Box::<[i32]>::deserialize_eps(cursor.as_bytes())?;
+    assert_eq!(b, a.as_ref());
+
+    cursor.set_position(0);
+    let d = Data {
+        a: vec![0, 1, 2, 3].into_boxed_slice(),
+        b: [1, 2, 3],
+    };
+    d.serialize(&mut cursor)?;
+    cursor.set_position(0);
+    let e = Data::<Box<[i32]>>::deserialize_full(&mut cursor)?;
+    assert_eq!(e, d);
+
+    cursor.set_position(0);
+    let d = Data { a, b: [1, 2, 3] };
+    d.serialize(&mut cursor)?;
+    cursor.set_position(0);
+    let e = Data::<Box<[i32]>>::deserialize_eps(cursor.as_bytes())?;
+    assert_eq!(e.a, d.a.as_ref());
+    Ok(())
+}
