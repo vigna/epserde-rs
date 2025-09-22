ab39df63b1fd505d7508adf0aae4da025a5126e5
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-09-22 18:43:03 +0200

    More detailed specs

---
+++ epserde/tests/fail/drop_memcase_deref.rs
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+use epserde::prelude::*;
+use std::io::Cursor;
+use std::ops::Deref;
+
+fn main() {
+    let v = vec![0, 10, 20, 30, 40];
+
+    let mut buffer = Vec::new();
+    unsafe { v.serialize(&mut buffer).unwrap() };
+    let cursor = Cursor::new(&buffer);
+    let mem_case = unsafe { Vec::<i32>::read_mem(cursor, buffer.len()).unwrap() };
+    let v = mem_case.deref();
+    drop(mem_case);
+    let _vv = v;
+}
