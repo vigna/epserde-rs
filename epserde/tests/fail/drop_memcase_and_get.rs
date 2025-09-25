8b89a09aef8d9a002874e49d63d1462fab468257
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-09-25 16:55:48 +0200

    Added trybuild test for references inside uncased instances

---
+++ epserde/tests/fail/drop_memcase_and_get.rs
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+use epserde::prelude::*;
+use std::io::Cursor;
+
+fn main() {
+    let vec = vec!["foo".to_string(), "bar".to_string(), "baz".to_string()];
+
+    let mut buffer = Vec::new();
+    unsafe { vec.serialize(&mut buffer).unwrap() };
+    let cursor = Cursor::new(&buffer);
+    let mem_case = unsafe { <Vec<String>>::read_mem(cursor, buffer.len()).unwrap() };
+
+    let s = mem_case.uncase().get(0);
+    drop(mem_case);
+    assert_eq!(s, Some(&"foo"));
+}
